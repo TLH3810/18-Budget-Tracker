6c98096d710b9c73781e198e762e1687207806ba
Yorkie Fan <terahowell@gmail.com>
2021-08-15 15:19:02 -0400

    adding in last service-worker funciton

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,40 +1,52 @@
-const FILES_TO_CASH= [
-  "/",
-  "/db.js",
-  "/index.js",
-  "/manifest.webmanifest",
-  "/styles.css",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+const FILES_TO_CASH = [
+    "/",
+    "/db.js",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ];
 
 const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
-self.addEventListener("install", function(event){
+self.addEventListener("install", function (event) {
     event.waitUntil(caches.open(CACHE_NAME)
-    .then(function(cache){
-        return cache.addAll (FILES_TO_CASH);
-    }));
+        .then(function (cache) {
+            return cache.addAll(FILES_TO_CASH);
+        }));
 }
 );
 
-self.addEventListener("fetch", function(event){
-    if (event.request.url.includes("/api/")){
+self.addEventListener("fetch", function (event) {
+    if (event.request.url.includes("/api/")) {
         event.respondWith(
             caches.open(DATA_CACHE_NAME)
-            .then(cache =>{
-                return fetch(event.request)
-                .then(responce => {
-                    if (responce.status === 200){
-                        cache.put(event.request.url, responce.clone())
-                    }
-                    return responce;
+                .then(cache => {
+                    return fetch(event.request)
+                        .then(responce => {
+                            if (responce.status === 200) {
+                                cache.put(event.request.url, responce.clone())
+                            }
+                            return responce;
+                        })
+                        .catch(err => { return cache.match(event.request); });
                 })
-                .catch(err =>{return cache.match(event.request);});
-            })
-            .catch(err => console.log(err))
+                .catch(err => console.log(err))
         );
         return;
     }
-})
+
+    event.respondWith(
+        fetch(event.request).catch(function() {
+          return caches.match(event.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (event.request.headers.get("accept").includes("text/html")) {
+              return caches.match("/");
+            }
+          });
+        })
+      );
+});
